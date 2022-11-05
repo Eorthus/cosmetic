53a11f05a2fa5e721ce7ebf93a28aebec615923a
eorthus <oakenshielddis@gmail.com>
2022-11-05 13:10:48 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+let images = document.querySelectorAll(".lazyload");
+if (images) {
+    lazyload(images);
+}
+
+$('.feedback-slider').slick({
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    arrows: true,
+    variableHeight: true,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: true,
+            }
+        },
+    ]
+});
+$('.hello-slider').slick({
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+        {
+            breakpoint: 750,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },
+    ]
+});
+$('.shop-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    Infinity: false,
+    responsive: [
+        {
+            breakpoint: 780,
+            settings: {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 690,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 410,
+            settings: {
+                slidesToShow: 1.2,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },
+    ]
+});
